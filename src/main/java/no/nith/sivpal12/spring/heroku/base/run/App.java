a3f0701b8732b345e51bcc61d3827655bb46fc31
Pal M Sivertsen <palmarius@hotmail.com>
2014-12-14 01:29:21 +0100

    Removed log4j

--- src/main/java/no/nith/sivpal12/spring/heroku/base/run/App.java
+++ src/main/java/no/nith/sivpal12/spring/heroku/base/run/App.java
@@ -2,12 +2,6 @@ package no.nith.sivpal12.spring.heroku.base.run;
 
 import java.io.IOException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.Filter.Result;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.filter.RegexFilter;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -18,8 +12,6 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class App {
-    private static final Logger LOG = LogManager.getLogger();
-
     private static final String CONTEXT_PATH = "/";
     private static final String CONFIG_PACKAGE_LOCATION = "no.nith.sivpal12.spring.heroku.base.spring.config";
     private static final String MAPPING_URL = "/";
@@ -28,8 +20,6 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-        configLog4j();
-
         int port;
         try {
             port = Integer.valueOf(System.getenv("PORT"));
@@ -37,20 +27,10 @@ public class App {
             port = 8080;
         }
 
-        LOG.info("Starting server");
         Server server = new Server(port);
         server.setHandler(getServletContextHandler(getContext()));
         server.start();
-        LOG.info("Server started");
         server.join();
-    }
-
-    private static void configLog4j() {
-        Configuration config = Configurator.initialize("minLogger", null).getConfiguration();
-        RegexFilter filter = RegexFilter.createFilter(
-                "no.nith.sivpal12.spring.heroku.base.spring",
-                Boolean.TRUE.toString(), Result.ACCEPT.name(), Result.ACCEPT.name());
-        config.addFilter(filter);
     }
 
     private static WebAppContext getServletContextHandler(WebApplicationContext context) throws IOException {
@@ -60,14 +40,12 @@ public class App {
         contextHandler.addServlet(new ServletHolder(new DispatcherServlet(context)), MAPPING_URL);
         contextHandler.addEventListener(new ContextLoaderListener(context));
         contextHandler.setResourceBase(new ClassPathResource("webapp").getURI().toString());
-        LOG.info("Context handler initialized");
         return contextHandler;
     }
 
     private static WebApplicationContext getContext() {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.setConfigLocation(CONFIG_PACKAGE_LOCATION);
-        LOG.info("Context initialized");
         return context;
     }
 
